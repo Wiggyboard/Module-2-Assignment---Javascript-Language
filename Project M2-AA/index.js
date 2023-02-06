22c4e9890e3089fe090fbe7599cc3e5c8266d297
Wiggyboard <105466565+Wiggyboard@users.noreply.github.com>
2023-02-05 17:51:49 -0800

    Initial commit

---
+++ Project M2-AA/index.js
@@ -0,0 +1,14 @@
+/* function greetTheStudent(studentName) {
+    return `Hello there ${studentName}`;
+} */
+
+const greetTheStudent = studentName => `Hello there ${studentName}`;
+
+const fullName = (firstName, middleName, lastName) => `${lastName}, ${middleName}. ${firstName}`;
+
+/* const student = "Mary Jane";
+const greet = greetTheStudent(student);
+console.log(greet); */
+
+const titleName = fullName("Mary", "R", "Jane");
+console.log(titleName);
